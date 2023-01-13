2e08ab683d1c79975c8c2997f925a9a8304aa49b
Madina Akhmedova <akhmedovamadina11@gmail.com>
2023-01-13 22:49:36 +0300

    17_6 page object hw

--- src/test/java/demoqa/AutomationPracticeFormTestCase.java
+++ src/test/java/demoqa/AutomationPracticeFormTestCase.java
@@ -1,12 +1,8 @@
 package demoqa;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTestCase extends TestBase {
 
@@ -17,41 +13,30 @@ public class AutomationPracticeFormTestCase extends TestBase {
         String userEmail = "akhmedovamadina11@gmail.com";
         String userNumber = "9991112233";
         String gender = "Female";
-
-        registrationPage.openPage();
+        String dayOfBirth = "06";
+        String monthOfBirth = "January";
+        String yearOfBirth = "1993";
+        String hobby = "Sports";
+        String imagePath = "test.png";
+        String state = "NCR";
+        String city = "Delhi";
+        String subject = "Maths";
 
         registrationPage.openPage()
+                .checkFormTitle("Student Registration Form")
                 .setFirstName(userFirstname)
                 .setLastName(userSurname)
                 .setEmail(userEmail)
                 .setGender(gender)
                 .setPhone(userNumber)
-                .setBirthDate("06", "January", "1993");
-        SelenideElement userForm = $("#userForm");
-
-        userForm.parent().$("h5").shouldHave(text("Student Registration Form"));
-
-        userForm.$("#hobbies-checkbox-1").parent().$("label").shouldBe(text("Sports"));
-        userForm.$("#hobbies-checkbox-1").parent().$("label").click();
-        userForm.$("#uploadPicture").uploadFromClasspath("test.png");
-
-        executeJavaScript("$('#adplus-anchor').remove();$('footer').remove();");
-
-        actions().moveToElement(userForm.$("#stateCity-wrapper").sibling(0)).perform();
-        $("#state").click();
-        $("#react-select-3-option-1").shouldBe(text("Uttar Pradesh")).click();
-        $("#city").click();
-        $("#react-select-4-option-1").shouldBe(text("Lucknow")).click();
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("January"); // select from months dropdown
-        $(".react-datepicker__year-select").selectOption("1993"); // select from years dropdown
-        $(".react-datepicker__month").$$(".react-datepicker__week")
-                .get(1).$$(".react-datepicker__day").findBy(text("6")).click(); // select the day
-
-        $("#subjectsInput").setValue("m");
-        $("#react-select-2-option-0").shouldBe(visible).shouldBe(text("Maths")).click();
-        $("#subjectsInput").pressEnter();
+                .setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setHobby(hobby)
+                .setState(state)
+                .setCity(city)
+                .setPicture(imagePath)
+                .setSubject(subject)
+                .submitForm()
+        ;
 
 
         registrationPage.verifyResultsModalAppears()
@@ -59,13 +44,12 @@ public class AutomationPracticeFormTestCase extends TestBase {
                 .verifyResult("Student Email", text(userEmail))
                 .verifyResult("Gender", text(gender))
                 .verifyResult("Mobile", text(userNumber))
-                .verifyResult("Date of Birth", text("06 January,1993"))
-                .verifyResult("Hobbies", text("Sports"))
-                .verifyResult("Subjects", text("Maths"))
-                .verifyResult("Picture", text("test.png"))
+                .verifyResult("Date of Birth", text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth))
+                .verifyResult("Hobbies", text(hobby))
+                .verifyResult("Subjects", text(subject))
+                .verifyResult("Picture", text(imagePath))
                 .verifyResult("Address", empty)
-                .verifyResult("State and City", text("Uttar Pradesh Lucknow"))
+                .verifyResult("State and City", text(state + " " + city))
         ;
-
     }
 }
--- src/test/java/demoqa/pages/RegistrationPage.java
+++ src/test/java/demoqa/pages/RegistrationPage.java
@@ -6,23 +6,28 @@ import demoqa.pages.components.CalendarComponent;
 import demoqa.pages.components.RegistrationResultsModal;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
-    private final String TITLE_TEXT = "Student Registration Form";
-    private CalendarComponent calendarComponent = new CalendarComponent();
-    private RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
-    private SelenideElement
+    private final CalendarComponent calendarComponent = new CalendarComponent();
+    private final RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
+    private final SelenideElement
             firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
-            dateOfBirthInput = $("#dateOfBirthInput");
+            dateOfBirthInput = $("#dateOfBirthInput"),
+            emailInput = $("#userEmail"),
+            genderInput = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            hobbyInput = $("#hobbiesWrapper"),
+            pictureInput = $("#uploadPicture");
 
     public RegistrationPage openPage() {
         open("/automation-practice-form");
+        String TITLE_TEXT = "Student Registration Form";
         $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT));
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove();$('#adplus-anchor').remove();$('footer').remove();");
 
         return this;
     }
@@ -46,19 +51,19 @@ public class RegistrationPage {
     }
 
     public RegistrationPage setEmail(String value) {
-        $("#userEmail").setValue(value); // todo move to Selenide elements
+        emailInput.setValue(value);
 
         return this;
     }
 
     public RegistrationPage setGender(String value) {
-        $("#genterWrapper").$(byText(value)).click(); // todo move to Selenide elements
+        genderInput.$(byText(value)).click();
 
         return this;
     }
 
     public RegistrationPage setPhone(String value) {
-        $("#userNumber").setValue(value); // todo move to Selenide elements
+        userNumberInput.setValue(value);
 
         return this;
     }
@@ -80,14 +85,48 @@ public class RegistrationPage {
 
         return this;
     }
-    /*
-    *** SELENIUM
-    @FindBy(how = How.XPATH, xpath = ".//*[@class='submit']")
-    public Button logoutBtn;
 
-    *** SELENIDE
-    public SelenideElement logoutBtn = $x(".//*[@class='submit]");
-    public SelenideElement logoutBtn = $(".submit");
-     */
+    public RegistrationPage setHobby(String value) {
+        hobbyInput.$(byText(value)).click();
 
+        return this;
+    }
+
+    public RegistrationPage setPicture(String path) {
+        pictureInput.uploadFromClasspath(path);
+
+        return this;
+    }
+
+    public RegistrationPage checkFormTitle(String value) {
+        $("#userForm").parent().$("h5").shouldHave(text(value));
+
+        return this;
+    }
+
+    public RegistrationPage setState(String value) {
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setCity(String value) {
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setSubject(String value) {
+        $("#subjectsInput").setValue("" + value.charAt(0));
+        $("#react-select-2-option-0").shouldBe(visible).shouldBe(text(value)).click();
+
+        return this;
+    }
+
+    public void submitForm() {
+//        $("#submit").click(); // Doesnt work!!!
+        $("#subjectsInput").pressEnter();
+    }
 }
